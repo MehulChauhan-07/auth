5e06c86cb93969a65fa9613ace851335beeb25b2
MehulChauhan-07 <mehulsinhchauhan5124@gmail.com>
2025-07-08 23:56:35 +0530

    add nodemailer configuration and email notification on user registration

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -1,7 +1,8 @@
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
 import userModel from "../models/user,model.js";
-import { response } from "express";
+import transporter from "../config/nodemailer.js"; // Assuming you have a nodemailer config file
+
 export const register = async (req, res) => {
   const { name, email, password } = req.body;
 
@@ -36,6 +37,22 @@ export const register = async (req, res) => {
       maxAge: 24 * 60 * 60 * 1000, // 1 day
     });
 
+    // ! sending mail
+    const mailOptions = {
+      from: process.env.SENDER_EMAIL,
+      to: email,
+      subject: "Welcome to Our Service",
+      text: `Hello ${name},\n\nThank you for registering with us using the this email : ${email}! We're excited to have you on board.\n\nBest regards,\nThe Team`,
+    };
+
+    await transporter.sendMail(mailOptions, (error, info) => {
+      if (error) {
+        console.error("Error sending email:", error);
+      } else {
+        console.log("Email sent successfully:", info.response);
+      }
+    });
+
     return res.json({
       success: true,
       message: "Registration successful",
@@ -51,12 +68,10 @@ export const login = async (req, res) => {
   const { email, password } = req.body;
 
   if (!email || !password) {
-    return res
-      .status(400)
-      .json({
-        success: false,
-        message: "EMail and password fields are required",
-      });
+    return res.status(400).json({
+      success: false,
+      message: "EMail and password fields are required",
+    });
   }
 
   try {
